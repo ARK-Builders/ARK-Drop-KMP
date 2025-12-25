4670f606b008a2130d49740bc85d9496fb6de5e5
mdrlzy <mordeniusss@gmail.com>
2025-12-25 10:26:29 +0600

    Android platform: add Room, DataStore

--- shared/src/androidMain/kotlin/dev/arkbuilders/drop/di/PlatformModule.android.kt
+++ shared/src/androidMain/kotlin/dev/arkbuilders/drop/di/PlatformModule.android.kt
@@ -1,9 +1,18 @@
 package dev.arkbuilders.drop.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import dev.arkbuilders.drop.data.db.DropDatabase
 import dev.arkbuilders.drop.data.helper.AvatarHelper
 import dev.arkbuilders.drop.data.helper.NetworkStatus
 import dev.arkbuilders.drop.data.helper.PermissionsHelper
 import dev.arkbuilders.drop.data.helper.ResourcesHelper
+import dev.arkbuilders.drop.data.settings.createDataStore
+import dev.arkbuilders.drop.data.settings.dataStoreFileName
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -13,4 +22,20 @@ actual val platformModule: Module = module {
     single { NetworkStatus(androidContext()) }
     single { PermissionsHelper(androidContext()) }
     single { ResourcesHelper(androidContext()) }
+
+    single<DropDatabase> {
+        val dbFile = androidApplication().getDatabasePath("drop.db")
+        Room.databaseBuilder<DropDatabase>(
+            context = androidApplication(),
+            name = dbFile.absolutePath,
+        ).setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
+            .build()
+    }
+
+    single<DataStore<Preferences>> {
+        createDataStore {
+            androidContext().filesDir.resolve(dataStoreFileName).absolutePath
+        }
+    }
 }
